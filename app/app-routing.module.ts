ce22cc5522c1d9dfb60dfac81ccd3700c44c3252
nrocha02 <thewolf2732@gmail.com>
2023-09-03 20:50:11 -0300

    Progreso

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,13 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'info',
+    loadChildren: () => import('./pages/info/info.module').then( m => m.InfoPageModule)
   },
 ];
 
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Componente{
+  name:string;
+  redirecTo: string;
+  icon:string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,4 +13,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+  componentes : Componente[]=[
+    {
+      name: 'Inicio',
+      redirecTo: '/inicio',
+      icon:'home-outline'
+    },
+    {
+      name: 'Login',
+      redirecTo: '/login',
+      icon:'person-add-outline'
+    },
+    {
+      name: 'Registro',
+      redirecTo: '/registro',
+      icon:'log-in-outline'
+    },
+    {
+      name: 'Información',
+      redirecTo: '/info',
+      icon:'help-circle-outline'
+    },
+
+  ]
 }
