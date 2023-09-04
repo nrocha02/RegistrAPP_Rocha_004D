b0fd0c525ccf92cf241936c3bbcbbc052245627f
nrocha02 <thewolf2732@gmail.com>
2023-09-03 23:52:06 -0300

    Casi terminado

--- app/pages/login/login.page.ts
+++ app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router , private alertController: AlertController) { }
+  usuario = {
+    email: '',
+    password: ''
+  };
 
   ngOnInit() {
   }
+  async error() {
+    const alert = await this.alertController.create({
+      header: 'Error al iniciar sesión',
+      message: 'usuario o contraseña incorrectas',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async bien() {
+    const alert = await this.alertController.create({
+      header: '¡Iniciando Sesion!',
+      message: '¡Hola de nuevo!',
+      buttons: ['OK'],
+    });
+    this.router.navigate(['/inicio']);
+    await alert.present();
+  }
+  
+  login() {
+    if (this.usuario.email === 'test' && this.usuario.password === '1234') {
+      this.bien();
+    } else {  
+      this.error();
+    }
+  }
+  
 
 }
--- app/pages/registro/registro.page.ts
+++ app/pages/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  usuario = {
+    nombre: '',
+    email: '',
+    password: '',
+  };
 
+  constructor(private alertController: AlertController, private router: Router) { }
   ngOnInit() {
+    
   }
+
+  async mostrarMensaje() {
+    const alert = await this.alertController.create({
+      header: '¡Gracias!',
+      message: '¡Sus datos han sido enviados!',
+      buttons: ['OK'],
+    });
+    this.router.navigate(['/inicio']);
+    await alert.present();
+  }
+
+  Enviar() {
+    this.mostrarMensaje();
+    this.usuario.nombre = '';
+    this.usuario.email = '';
+    this.usuario.password = '';
+  }
+ 
 
 }
